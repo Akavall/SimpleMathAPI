37392eaf9abfe8eb74e7db8babc38c519c79bb1a
Kirill <Akavall@gmail.com>
2016-05-01 10:26:03 -0500

    changed hi to hello

--- simple_math.go
+++ simple_math.go
@@ -46,7 +46,7 @@ func is_prime_wrapper(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fmt.Println("Hi")
+	fmt.Println("Hello")
 
 	// We need need to create a file manually
 	// and change the permissions with: 
