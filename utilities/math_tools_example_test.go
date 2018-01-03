2fda980456e2bc8e1435e2821120f4f24164c809
Kirill <Akavall@gmail.com>
2018-01-02 20:30:28 -0800

    added input Form check to see, if fails it returns 400 error, and mostly as toy added defer/recover combo to return 500 status if isPrime fails

--- utilities/math_tools_example_test.go
+++ utilities/math_tools_example_test.go
@@ -9,8 +9,8 @@ func ExampleIsPrime() {
 	fmt.Println(IsPrime(19))
 	fmt.Println(IsPrime(1))
 	// Output:
-	// 2 false
-	// 0 true
-	// 0 false
+	// 2 false <nil>
+	// 0 true <nil>
+	// 0 false <nil>
 
 }
--- utilities/math_tools_test.go
+++ utilities/math_tools_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 func TestIsPrime(t *testing.T) {
-	first_primes := []int {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53}
-	primes_map := map[int]bool {}
+	first_primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}
+	primes_map := map[int]bool{}
 	for _, ele := range first_primes {
 		primes_map[ele] = true
 	}
 
 	for i := 2; i < 54; i++ {
-		div, is_prime := IsPrime(i)
+		div, is_prime, _ := IsPrime(i)
 		_, in_map := primes_map[i]
 		if is_prime != in_map {
 			t.Errorf("%d, is_prime: %t, prime_map: %t", i, is_prime, in_map)
 			t.Errorf("%d, has a non-zero divisor for prime number: %d", i, div)
-		} 
+		}
 		if is_prime == false {
-			if (i / div) * div != i {
+			if (i/div)*div != i {
 				t.Errorf("%d gets div: %d, which does not divide number evenly", i, div)
 			}
 		}
