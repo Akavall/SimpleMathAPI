9926dab51615a673ffab85b087125d937774570b
Kirill <Akavall@gmail.com>
2018-09-02 20:05:06 -0700

    Revert "breaking the unit tests"
    This reverts commit 044e8efafac54a2bde92ce5a40a327cd5ac74838.

--- utilities/math_tools_test.go
+++ utilities/math_tools_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestIsPrime(t *testing.T) {
-	first_primes := []int{0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}
-	primes_map := map[int]bool{}
+	first_primes := []int {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53}
+	primes_map := map[int]bool {}
 	for _, ele := range first_primes {
 		primes_map[ele] = true
 	}
