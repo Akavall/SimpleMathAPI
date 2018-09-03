e684dbef519e593e95ef4a4b1a9b0604f8941aa4
Kirill <Akavall@gmail.com>
2018-09-02 21:08:18 -0700

    Revert "trying to merge extra functionality from master, running into challenges..."
    This reverts commit 83c9ea1e0d7a956d044b446e198ba3d31b42ed56.

--- utilities/math_tools.go
+++ utilities/math_tools.go
@@ -1,39 +1,24 @@
 package math_tools
 
 import (
-	"errors"
-	"fmt"
 	"math"
 )
 
 // Chech whether the integer is prime
 // returns true if integer is prime
-// returns false if integer is not prime
-func IsPrime(x int) (largest_div int, isPrime bool, err error) {
-
-	defer func() {
-		if r := recover(); r != nil {
-			largest_div = 0
-			isPrime = false
-			err = errors.New(fmt.Sprintf("%s", r))
-		}
-	}()
-
-	if x < 2 {
-		return 0, false, nil
-	}
-	if x == 2 {
-		return 0, true, nil
-	}
-	if x%2 == 0 {
-		return 2, false, nil
+// returns false if integer is not prime 
+func IsPrime(x int) bool {
+	if x < 2 { return false }
+	if x == 2 { return true }
+	if x % 2 == 0 { 
+		return false
 	} else {
-		limit := int(math.Sqrt(float64(x))) + 1
+		limit := int(math.Sqrt(float64(x))) + 2
 		for i := 3; i < limit; i += 2 {
-			if x%i == 0 {
-				return i, false, nil
+			if x % i == 0 {
+				return false
 			}
 		}
 	}
-	return 0, true, nil
+	return true 
 }
