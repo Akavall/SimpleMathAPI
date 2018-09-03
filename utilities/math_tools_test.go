693dad00e53cf213fbeab1846cfba6e0d72da35e
Kirill <Akavall@gmail.com>
2018-09-02 20:09:03 -0700

    added print line FAIL

--- utilities/math_tools_test.go
+++ utilities/math_tools_test.go
@@ -1,15 +1,18 @@
 package math_tools
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestIsPrime(t *testing.T) {
-	first_primes := []int {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53}
-	primes_map := map[int]bool {}
+	first_primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}
+	primes_map := map[int]bool{}
 	for _, ele := range first_primes {
 		primes_map[ele] = true
 	}
+
+	fmt.Println("FAIL")
 
 	for i := 0; i < 54; i++ {
 		is_prime := IsPrime(i)
